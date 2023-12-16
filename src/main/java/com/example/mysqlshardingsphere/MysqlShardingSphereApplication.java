59b6b4f169f059b2f1da0f11cab03c5958f6d353
dongwei <964013060@qq.com>
2023-12-16 15:36:42 +0800

    初始化设置

---
+++ src/main/java/com/example/mysqlshardingsphere/MysqlShardingSphereApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlshardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlShardingSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlShardingSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mysqlshardingsphere/MysqlShardingSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlshardingsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlShardingSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
