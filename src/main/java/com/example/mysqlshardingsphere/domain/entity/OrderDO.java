3d01f0d8bfffd737be1bc19a39194f753bfe0a17
dongwei <964013060@qq.com>
2024-01-14 22:49:56 +0800

    安装mybatis-plus依赖

---
+++ src/main/java/com/example/mysqlshardingsphere/domain/entity/OrderDO.java
@@ -0,0 +1,14 @@
+package com.example.mysqlshardingsphere.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+
+/**
+ * @Author wei.dong
+ * @Date 2024/1/14 15:29
+ * @Version 1.0.0
+ */
+
+
+public class OrderDO {
+
+}
