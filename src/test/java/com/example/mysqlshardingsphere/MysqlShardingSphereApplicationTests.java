d5f24492a002bb5e957df3423a3c442443262c7b
dongwei <964013060@qq.com>
2024-03-05 21:34:03 +0800

    校验正则表达式

--- src/test/java/com/example/mysqlshardingsphere/MysqlShardingSphereApplicationTests.java
+++ src/test/java/com/example/mysqlshardingsphere/MysqlShardingSphereApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.mysqlshardingsphere;
 
 import com.example.mysqlshardingsphere.domain.entity.OrderDO;
 import com.example.mysqlshardingsphere.domain.mapper.OrderMapper;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class MysqlShardingSphereApplicationTests {
@@ -12,13 +16,25 @@ class MysqlShardingSphereApplicationTests {
 	private OrderMapper orderMapper;
 
 	@Test
-	void contextLoads() {
-		for (int i = 0; i < 100; i++) {
-			OrderDO orderDO = new OrderDO();
-			orderDO.setOrderId(i);
-			orderDO.setOrderName("订单序号"+i);
-			orderMapper.insert(orderDO);
+	void contextLoads() throws Exception{
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("11","11");
+		System.out.println(jsonObject);
+
+	}
+
+	@Test
+	void checkReginStr(){
+		String str = "a b";
+		String reg = "\\b[a-zA-Z]\\b";
+		Pattern p = Pattern.compile(reg);
+		Matcher m = p.matcher(str);
+		System.out.println(m.find());
+
+		if (m.find()){
+			System.out.println("regin find:"+m.group());
 		}
+
 	}
 
 }
